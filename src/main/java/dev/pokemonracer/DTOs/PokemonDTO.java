a124a0e18dbb72b2e18318f4ce85784d4a879279
Jonathan <510031@student.fontys.nl>
2023-11-28 22:07:28 +0100

    Started on Websockets and added auth0 frontend only

--- src/main/java/dev/pokemonracer/DTOs/PokemonDTO.java
+++ src/main/java/dev/pokemonracer/DTOs/PokemonDTO.java
@@ -1,6 +1,6 @@
 package dev.pokemonracer.DTOs;
+import dev.pokemonracer.model.Pokemon;
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Size;
 
 public class PokemonDTO {
 
@@ -9,33 +9,36 @@ public class PokemonDTO {
     
     @NotEmpty
     private String name;
-    
-    @NotEmpty
-    private String type;
-    
+
     @NotEmpty
     private String imageString;
 
-    public PokemonDTO(int id, String name, String type, String imageString) {
+    public PokemonDTO(int id, String name, String imageString) {
         this.id = id;
         this.name = name;
-        this.type = type;
         this.imageString = imageString;
     }
-
     public int getId() {
         return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getName() {
         return name;
     }
 
-    public String getType() {
-        return type;
+    public void setName(String name) {
+        this.name = name;
     }
-    
+
     public String getImageString() {
         return imageString;
+    }
+
+    public void setImageString(String imageString) {
+        this.imageString = imageString;
     }
 }
