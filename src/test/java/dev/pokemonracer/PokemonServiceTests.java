d799fc2b6808e48ce98a2fc8d1653d4e516d25e7
Jonathan <510031@student.fontys.nl>
2023-12-05 13:58:48 +0100

    trying building again

--- src/test/java/dev/pokemonracer/PokemonServiceTests.java
+++ src/test/java/dev/pokemonracer/PokemonServiceTests.java
@@ -14,6 +14,47 @@ import dev.pokemonracer.service.PokeAPIservice;
 @SpringBootTest
 class PokemonracerApplicationTests {
 
-	
+	@Test
+    void resetPokemon_removePokemonIDFromSet_shouldBeRemovedFromSet() {
+        // arrange
+        var pokeAPIservice = new PokeAPIservice(null);
+        Set<Integer> generatedPokemonIds = new HashSet<>();
+        generatedPokemonIds.add(1);
+        generatedPokemonIds.add(2);
+        generatedPokemonIds.add(3);
+        pokeAPIservice.setGeneratedPokemonIds(generatedPokemonIds);
+
+        // act
+        pokeAPIservice.resetGuessedPokemonList();
+
+        // assert
+        Assertions.assertThat(pokeAPIservice.getGeneratedPokemonIds()).isEmpty();
+    }
+
+	@ParameterizedTest
+    @CsvSource({
+            "0, 1, 1010",
+            "1, 1, 151",
+            "2, 151, 251",
+            "3, 252, 386",
+            "4, 387, 493",
+            "5, 494, 649",
+            "6, 650, 721",
+            "7, 722, 809",
+            "8, 809, 898",
+            "9, 899, 1010"
+    })
+    void getRandomPokemonID_generateRandomPokemonId_shouldBeInSpecifiedRange(int generation, int lowerBound, int upperBound) {
+        // arrange
+        var pokeAPIservice = new PokeAPIservice(null);
+
+        // act
+        int id = pokeAPIservice.generateRandomPokemonId(generation);
+
+        // assert
+        Assertions.assertThat(id).isBetween(lowerBound, upperBound);
+        Assertions.assertThat(pokeAPIservice.getPokemonGeneration()).isEqualTo(generation);
+        Assertions.assertThat(id).isPositive();
+    }
 
 }
