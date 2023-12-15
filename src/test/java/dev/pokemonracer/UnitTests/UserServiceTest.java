18fdb9e5ef34e8d7c6b2f629fd2f18b7006718cb
Jonathan <510031@student.fontys.nl>
2023-12-15 14:26:21 +0100

    changed main.yml again v5

--- src/test/java/dev/pokemonracer/UnitTests/UserServiceTest.java
+++ src/test/java/dev/pokemonracer/UnitTests/UserServiceTest.java
@@ -30,78 +30,74 @@ public class UserServiceTest {
 
     @Test
     public void CreateUser_UserDoesNotExist_UserShouldBeCreated() {
-        // Create a sample user
+        // Arrange
         User user = new User();
         user.setAuth0Id("auth0id123");
         user.setEmail("test@example.com");
 
-        // Mock the behavior of UserRepository.save()
         when(userRepository.save(any(User.class))).thenReturn(user);
 
-        // Call the method to be tested
+        // Act
         userService.createUser(user);
 
-        // Verify that the save method was called with the correct user
+        // Assert
         verify(userRepository).save(user);
     }
 
     @Test
     public void GetUserByAuth0Id_UserExists_UserShouldBeReturned() {
-        // Create a sample user
+        // Arrange
         User user = new User();
         user.setAuth0Id("auth0id123");
 
-        // Mock the behavior of UserRepository.findByAuth0Id()
         when(userRepository.findByAuth0Id("auth0id123")).thenReturn(user);
 
-        // Call the method to be tested
+        // Act
         User result = userService.getUserByAuth0Id("auth0id123");
 
-        // Verify that the correct user was returned
+        // Assert
         assertEquals(user, result);
     }
 
     @Test
     public void GetUserById_UserExists_UserShouldBeReturned() {
-        // Create a sample user
+        // Arrange
         User user = new User();
         user.setId(1L);
 
-        // Mock the behavior of UserRepository.findById()
         when(userRepository.findById(1L)).thenReturn(java.util.Optional.of(user));
 
-        // Call the method to be tested
+        // Act
         User result = userService.getUserById(1L);
 
-        // Verify that the correct user was returned
+        // Assert
         assertEquals(user, result);
     }
 
     @Test
     public void GetUserByEmail_UserExists_UserShouldBeReturned() {
-        // Create a sample user
+        // Arrange
         User user = new User();
         user.setEmail("test@example.com");
 
-        // Mock the behavior of UserRepository.findUserByEmail()
         when(userRepository.findUserByEmail("test@example.com")).thenReturn(user);
 
-        // Call the method to be tested
+        // Act
         User result = userService.getUserByEmail("test@example.com");
 
-        // Verify that the correct user was returned
+        // Assert
         assertEquals(user, result);
     }
 
     @Test
     public void GetUserByAuth0Id_UserDoesNotExist_ShouldReturnNull() {
-        // Mock the behavior of UserRepository.findByAuth0Id() when user is not found
+        // Arrange
         when(userRepository.findByAuth0Id("nonexistent")).thenReturn(null);
 
-        // Call the method to be tested
+        // Act
         User result = userService.getUserByAuth0Id("nonexistent");
 
-        // Verify that null is returned for a non-existent user
+        // Assert
         assertNull(result);
     }
 
